3a3b99ac248b392cc8223a3a2ef9f0862c4eed57
Jos Elstgeest <jos@tidi.nl>
2015-12-08 00:08:56 +0100

    add dockblock to constructor

--- src/PWElement.php
+++ src/PWElement.php
@@ -6,6 +6,12 @@ class PWElement
     public $str;
     public $flags;
 
+    /**
+     * PWElement constructor.
+     *
+     * @param string $str
+     * @param int    $flags
+     */
     public function __construct($str, $flags)
     {
         $this->str   = $str;
